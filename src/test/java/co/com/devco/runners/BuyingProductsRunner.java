d8136ccd1c51b15bc128833281e5eff2cdeb2e4a
Sergio Orozco <sergio_nea@milli.bank>
2024-09-23 17:36:52 -0500

    Updates to serenity 4

--- src/test/java/co/com/devco/runners/BuyingProductsRunner.java
+++ src/test/java/co/com/devco/runners/BuyingProductsRunner.java
@@ -1,15 +1,15 @@
 package co.com.devco.runners;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        features = "src/test/resources/features/buying_products.feature",
-        glue = "co.com.devco.stepdefinitions",
-        snippets = CucumberOptions.SnippetType.CAMELCASE
-)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,json:build/test-results/cucumber/frontend/global/results.json,timeline:build/test-results/timeline")
 public class BuyingProductsRunner {
 }
